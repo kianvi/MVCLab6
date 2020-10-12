66c80bf49e560163207ca94064e1c32dc783435b
Anton Kim <ki-an-vi@mail.ru>
2020-10-12 10:28:44 +0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is ASP.Net MVC Filters Tutorial";
+        }
+        [ActionName("CurrentTime")]
+        public string GetCurrentTime()
+        {
+        
+         return DateTime.Now.ToString("T");
+        }
+        [NonAction]
+        public string TimeString()
+        {
+            return "Time is " + DateTime.Now.ToString("T");
+        }
+
+    }
+}
